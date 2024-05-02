e47c7a966b2885cf3f59fdf2e4222e261553af93
Martin Weingart <martinweingart@gmail.com>
2024-05-02 09:21:31 -0300

    Update dev environment app to test tours

--- dev/App/Main/Main.tsx
+++ dev/App/Main/Main.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment, useContext, useEffect, useState } from 'react';
 import styles from './Main.module.scss';
 import {
   SFSettings,
@@ -19,31 +19,47 @@ import {
   UserContext,
   TimezonesContext,
   SFTopBar,
-  LARGE_SCREEN
+  LARGE_SCREEN,
+  ToursReminderTooltip,
+  ToursCarrouselModal,
+  Tour,
+  TourResumeTab,
+  TourContext,
+  useIsTourResumeOpen
 } from '../../../src';
-import { SFSpinner, useSFMediaQuery } from 'sfui';
+import { SFIcon, SFSpinner, SFText, useSFMediaQuery } from 'sfui';
 import { BASE_URL } from '../App';
 import { getTimezones } from '../../../src/Services/TimezoneService';
 import { logout } from '../../../src/Services/AuthService';
+import { TOURS } from '../tours';
 
 export const Main = (): React.ReactElement<{}> => {
-  const [isLoading, setIsLoading] = React.useState<boolean>(true);
-  const { setUser, setBusinessCardSettings } = React.useContext(UserContext);
-  const { setAreas } = React.useContext(AreasContext);
-  const { setCustomer } = React.useContext(CustomerContext);
-  const { setSubscriptions } = React.useContext(SubscriptionContext);
-  const { setTimezones } = React.useContext(TimezonesContext);
-  const [selectedSectionName, setSelectedSectionName] = React.useState<
+  const {
+    onStart: onTourStart,
+    onEnd: onTourEnd,
+    tour: activeTour,
+    isFeatureReminderOpen,
+    setIsFeatureReminderOpen
+  } = useContext(TourContext);
+  const { setUser, setBusinessCardSettings } = useContext(UserContext);
+  const { setAreas } = useContext(AreasContext);
+  const { setCustomer } = useContext(CustomerContext);
+  const { setSubscriptions } = useContext(SubscriptionContext);
+  const { setTimezones } = useContext(TimezonesContext);
+  const isBigScreen: boolean = useSFMediaQuery(LARGE_SCREEN);
+
+  const [selectedSectionName, setSelectedSectionName] = useState<
     string | undefined
   >();
-
-  const isBigScreen: boolean = useSFMediaQuery(LARGE_SCREEN);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [isToursCarrouselOpen, setIsToursCarrouselOpen] =
+    useState<boolean>(false);
 
   const onSettingsError = (e: SettingsError) => console.error(e);
   const onHome = () => console.log('onHome');
   const onUpgrade = () => console.log('onUpgrade');
 
-  React.useEffect(() => {
+  useEffect(() => {
     const init = async () => {
       setIsLoading(true);
       const userData: User = await getUser(BASE_URL);
@@ -83,33 +99,97 @@ export const Main = (): React.ReactElement<{}> => {
 
   const onMenuButtonClick = () => console.log('Open menu');
 
+  const onGotIt = (checked: boolean) => {
+    if (checked) {
+      console.log("Save don't show again status");
+    }
+    setIsFeatureReminderOpen(false);
+  };
+
+  const onCloseTourCarrousel = () => {
+    setIsFeatureReminderOpen(true);
+    setIsToursCarrouselOpen(false);
+  };
+
+  const onExitTourResume = () => {
+    setIsFeatureReminderOpen(true);
+    onTourEnd();
+  };
+
+  const onStartTour = (tour: Tour) => {
+    setIsToursCarrouselOpen(false);
+
+    if (tour.id === 1) {
+      setSelectedSectionName('agency');
+      onTourStart(tour);
+    }
+  };
+
+  const onResumeTour = () => {
+    onStartTour(activeTour as Tour);
+  };
+
+  const isTourResumeOpen = useIsTourResumeOpen();
+
   return (
     <React.Fragment>
       {isLoading && <SFSpinner />}
       {!isLoading && (
-        <div className={styles.main}>
-          <SFTopBar
-            enviroment="local"
-            product="cc"
-            siteTitle="Settings"
-            isBottomTitleVisible={!isBigScreen}
-            onLogout={onLogout}
-            onMenuButtonClick={onMenuButtonClick}
+        <Fragment>
+          {isTourResumeOpen && (
+            <TourResumeTab onExit={onExitTourResume} onResume={onResumeTour} />
+          )}
+
+          <ToursCarrouselModal
+            tours={TOURS}
+            open={isToursCarrouselOpen}
+            onClose={onCloseTourCarrousel}
+            onStart={onStartTour}
           />
 
-          <SFSettings
-            product="cc"
-            enviroment="local"
-            stripeApiKey={
-              'pk_test_51MEZItJHbTAgxqXa6dzvaI4SubteHn7zemB9uj6hXqltKSoEAPKvBRlMeHvn06fR03vqKFkegkmH0QWdkPrpbuGe00CkvRGgxb'
-            }
-            selectedSectionName={selectedSectionName}
-            onError={onSettingsError}
-            onHome={onHome}
-            onUpgrade={onUpgrade}
-            onSectionChange={(name) => setSelectedSectionName(name)}
-          />
-        </div>
+          <div className={styles.main}>
+            <SFTopBar
+              enviroment="local"
+              product="cc"
+              siteTitle="Settings"
+              isBottomTitleVisible={!isBigScreen}
+              onLogout={onLogout}
+              onMenuButtonClick={onMenuButtonClick}
+            />
+
+            <div className={styles.content}>
+              <div className={styles.leftPanel}>
+                <ToursReminderTooltip
+                  open={isFeatureReminderOpen}
+                  onGotIt={onGotIt}
+                >
+                  <div
+                    className={styles.tours}
+                    onClick={() => setIsToursCarrouselOpen(true)}
+                  >
+                    <SFIcon icon="Circle-Star" />
+                    <SFText type="component-1" sfColor="neutral">
+                      Feature Tours
+                    </SFText>
+                  </div>
+                </ToursReminderTooltip>
+              </div>
+
+              <SFSettings
+                product="cc"
+                enviroment="local"
+                stripeApiKey={
+                  'pk_test_51MEZItJHbTAgxqXa6dzvaI4SubteHn7zemB9uj6hXqltKSoEAPKvBRlMeHvn06fR03vqKFkegkmH0QWdkPrpbuGe00CkvRGgxb'
+                }
+                selectedSectionName={selectedSectionName}
+                onError={onSettingsError}
+                onHome={onHome}
+                onUpgrade={onUpgrade}
+                onSectionChange={(name) => setSelectedSectionName(name)}
+              />
+            </div>
+          </div>
+        </Fragment>
       )}
     </React.Fragment>
   );

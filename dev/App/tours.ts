86de4b1eb26bda6c2365a903692e5bb7cc66dd5e
Martin Weingart <martinweingart@gmail.com>
2024-05-14 09:59:09 -0300

    Add area tour

--- dev/App/tours.ts
+++ dev/App/tours.ts
@@ -24,5 +24,11 @@ export const TOURS: Tour[] = [
     title: 'Bussines Card',
     description: 'Personalize your digital business card.',
     link: 'https://www.youtube.com/embed/A_T2h-sbQcs'
+  },
+  {
+    id: 5,
+    title: 'Agency Areas',
+    description: 'Define agency areas by dragging and dropping.',
+    link: ''
   }
 ];

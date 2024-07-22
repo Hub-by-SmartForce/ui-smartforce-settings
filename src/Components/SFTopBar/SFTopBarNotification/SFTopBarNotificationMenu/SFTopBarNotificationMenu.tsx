84670fced4ef624c5f3ffffa65d0985b4e87bfb0
Martin Weingart <martinweingart@gmail.com>
2024-07-22 08:31:52 -0300

    Chip filters added to the notifications popover

--- src/Components/SFTopBar/SFTopBarNotification/SFTopBarNotificationMenu/SFTopBarNotificationMenu.tsx
+++ src/Components/SFTopBar/SFTopBarNotification/SFTopBarNotificationMenu/SFTopBarNotificationMenu.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './SFTopBarNotificationMenu.module.scss';
 import { SFButton, SFDivider } from 'sfui';
+import { FilterOption } from './FilterOption/FilterOption';
 import { AppNotification } from '../../../../Models';
 
 export interface SFTopBarNotificationMenuProps {
@@ -12,13 +13,29 @@ export const SFTopBarNotificationMenu = ({
   notifications,
   unreadNotifications
 }: SFTopBarNotificationMenuProps): React.ReactElement<SFTopBarNotificationMenuProps> => {
-  //TODO remove
-  console.log(notifications);
+  const [filterUnread, setFilterUnread] = useState<boolean>(false);
 
   return (
     <div className={styles.sFTopBarNotificationMenu}>
       <div className={styles.topActions}>
-        <div className={styles.filters}>// TODO add chip filters</div>
+        <div className={styles.filters}>
+          <FilterOption
+            label="All"
+            selected={!filterUnread}
+            onClick={() => setFilterUnread(false)}
+          />
+
+          <FilterOption
+            label={`Unread${
+              unreadNotifications.length > 0
+                ? ` (${unreadNotifications.length})`
+                : ''
+            }`}
+            selected={filterUnread}
+            disabled={unreadNotifications.length === 0}
+            onClick={() => setFilterUnread(true)}
+          />
+        </div>
 
         <SFButton
           variant="text"

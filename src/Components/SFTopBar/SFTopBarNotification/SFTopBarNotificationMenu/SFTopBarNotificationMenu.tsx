47738f2cb9fd5e946c2720dabf165a8e2d79ed14
Martin Weingart <martinweingart@gmail.com>
2024-07-25 14:35:29 -0300

    Empty notification list message

--- src/Components/SFTopBar/SFTopBarNotification/SFTopBarNotificationMenu/SFTopBarNotificationMenu.tsx
+++ src/Components/SFTopBar/SFTopBarNotification/SFTopBarNotificationMenu/SFTopBarNotificationMenu.tsx
@@ -19,46 +19,57 @@ export const SFTopBarNotificationMenu = ({
   onReadAll
 }: SFTopBarNotificationMenuProps): React.ReactElement<SFTopBarNotificationMenuProps> => {
   const [filterUnread, setFilterUnread] = useState<boolean>(false);
+  const isEmpty = notifications.length === 0;
 
   return (
-    <div className={styles.sFTopBarNotificationMenu}>
-      <div className={styles.topActions}>
-        <div className={styles.filters}>
-          <FilterOption
-            label="All"
-            selected={!filterUnread}
-            onClick={() => setFilterUnread(false)}
+    <div
+      className={`${styles.sFTopBarNotificationMenu} ${
+        isEmpty ? styles.isEmpty : ''
+      }`}
+    >
+      {isEmpty && <div>There are no notifications at the moment.</div>}
+
+      {!isEmpty && (
+        <>
+          <div className={styles.topActions}>
+            <div className={styles.filters}>
+              <FilterOption
+                label="All"
+                selected={!filterUnread}
+                onClick={() => setFilterUnread(false)}
+              />
+
+              <FilterOption
+                label={`Unread${
+                  unreadNotifications.length > 0
+                    ? ` (${unreadNotifications.length})`
+                    : ''
+                }`}
+                selected={filterUnread}
+                disabled={unreadNotifications.length === 0}
+                onClick={() => setFilterUnread(true)}
+              />
+            </div>
+
+            <SFButton
+              variant="text"
+              sfColor="blue"
+              size="medium"
+              disabled={unreadNotifications.length === 0}
+              onClick={onReadAll}
+            >
+              Mark all as read
+            </SFButton>
+          </div>
+
+          <SFDivider size={1} />
+
+          <NotificationList
+            list={filterUnread ? unreadNotifications : notifications}
+            onOpen={onOpen}
           />
-
-          <FilterOption
-            label={`Unread${
-              unreadNotifications.length > 0
-                ? ` (${unreadNotifications.length})`
-                : ''
-            }`}
-            selected={filterUnread}
-            disabled={unreadNotifications.length === 0}
-            onClick={() => setFilterUnread(true)}
-          />
-        </div>
-
-        <SFButton
-          variant="text"
-          sfColor="blue"
-          size="medium"
-          disabled={unreadNotifications.length === 0}
-          onClick={onReadAll}
-        >
-          Mark all as read
-        </SFButton>
-      </div>
-
-      <SFDivider size={1} />
-
-      <NotificationList
-        list={filterUnread ? unreadNotifications : notifications}
-        onOpen={onOpen}
-      />
+        </>
+      )}
     </div>
   );
 };

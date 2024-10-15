dc3ba26e561d7a7da1adbd6fca6f393f9814ea01
Martin Weingart <martinweingart@gmail.com>
2024-10-15 15:48:13 -0300

    Update options in member item and add title information

--- src/Models/Member.ts
+++ src/Models/Member.ts
@@ -1,4 +1,5 @@
 import { Pagination } from './Pagination';
+import { UserTitle } from './User';
 
 export type GetMemberType = 'invitations' | 'members';
 
@@ -35,4 +36,5 @@ export interface Member {
     | 'Account not verified'
     | 'Account not created';
   groups: MemberGroup[];
+  title?: UserTitle;
 }
